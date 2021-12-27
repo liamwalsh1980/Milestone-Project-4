30a146896c94fef09ed884bc4386357e17fa82ab
Liam Walsh <lwalsh_1980@hotmail.co.uk>
2021-12-27 18:01:12 +0000

    Added home app and templates successfully

--- the_bike_shop/settings.py
+++ the_bike_shop/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+    'home',
 ]
 
 MIDDLEWARE = [
@@ -59,7 +60,12 @@ ROOT_URLCONF = 'the_bike_shop.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            # route template directory
+            os.path.join(BASE_DIR, 'templates'),
+            # custome allauth directory to the templates dirs setting
+            os.path.join(BASE_DIR, 'templates', 'allauth'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
