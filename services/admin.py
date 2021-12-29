97e9847f36666b00576368ef54283dcb7594dc18
Liam Walsh <lwalsh_1980@hotmail.co.uk>
2021-12-29 13:18:57 +0000

    added services app, model and fixtures

---
+++ services/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import Service, ServiceCategory
+
+# Register your models here.
+admin.site.register(Service)
+admin.site.register(ServiceCategory)
---
+++ services/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+
+class ServiceCategory(models.Model):
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
+class Service(models.Model):
+    category = models.ForeignKey('ServiceCategory', null=True, blank=True, on_delete=models.SET_NULL)
+    name = models.CharField(max_length=254)
+    description = models.TextField()
+    rating = models.DecimalField(max_digits=3, decimal_places=1, null=True, blank=True)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
