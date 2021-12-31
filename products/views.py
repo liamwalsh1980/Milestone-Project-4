fc839acc5d8bea02eae9756e6215820c797af74c
Liam Walsh <lwalsh_1980@hotmail.co.uk>
2021-12-31 13:38:29 +0000

    Added services page links

--- products/views.py
+++ products/views.py
@@ -3,7 +3,7 @@ from django.contrib import messages
 # Used to handle complex query in the database to achieve 'or' logic
 # To match either from product name or product description - use Q
 from django.db.models import Q
-from .models import Product
+from .models import Product, Category
 
 # Create your views here.
 
@@ -13,8 +13,14 @@ def all_products(request):
 
     products = Product.objects.all()
     query = None
+    categories = None
 
     if request.GET:
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            products = products.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
+
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
@@ -29,6 +35,7 @@ def all_products(request):
     context = {
         'products': products,
         'search_term': query,
+        'current_categories': categories,
     }
 
     return render(request, 'products/products.html', context)
--- services/views.py
+++ services/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Service
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+# Used to handle complex query in the database to achieve 'or' logic
+# To match either from product name or product description - use Q
+from django.db.models import Q
+from .models import Service, ServiceCategory
 
 # Create your views here.
 
@@ -8,9 +12,30 @@ def all_services(request):
     """ A view to show all services, including sorting and search queries """
 
     services = Service.objects.all()
+    query = None
+    servicescategories = None
+
+    if request.GET:
+        if 'servicecategory' in request.GET:
+            servicescategories = request.GET['servicecategory'].split(',')
+            services = services.filter(image__name__in=servicescategories)
+            servicescategories = ServiceCategory.objects.filter(name__in=servicescategories)
+
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You didn't enter any search criteria!")
+                return redirect(reverse('services'))
+
+            # Pipe gives the 'or' statement
+            # 'i' makes both name and description case insensitive
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            services = services.filter(queries)
 
     context = {
         'services': services,
+        'search_term': query,
+        'current_servicecategories': servicescategories,
     }
 
     return render(request, 'services/services.html', context)
