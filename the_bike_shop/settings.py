7e58a61555df4e7fcef43f96bfbe5428d9dce905
Liam Walsh <lwalsh_1980@hotmail.co.uk>
2022-01-04 21:16:53 +0000

    Added cart context and delivery threshold logic

--- the_bike_shop/settings.py
+++ the_bike_shop/settings.py
@@ -76,6 +76,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',  # required by allauth
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'cart.contexts.cart_contents',
             ],
         },
     },
@@ -159,3 +160,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+FREE_DELIVERY_THRESHOLD = 6
+STANDARD_DELIVERY_PERCENTAGE = 10
