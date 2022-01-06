527aeb2b00aff3382c77898eee2bb2bb3c7c1378
Liam Walsh <lwalsh_1980@hotmail.co.uk>
2022-01-06 21:56:28 +0000

    Added sizes and colours to product model and template

--- products/models.py
+++ products/models.py
@@ -20,6 +20,8 @@ class Product(models.Model):
     category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
     name = models.CharField(max_length=254)
     description = models.TextField()
+    has_sizes = models.BooleanField(default=False, null=True, blank=True)
+    has_colors = models.BooleanField(default=True, null=True, blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     rating = models.DecimalField(max_digits=3, decimal_places=1, null=True, blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
