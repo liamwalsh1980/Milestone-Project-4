c62a60b3deee83988a5e7db7f352085da9a7e2df
Liam Walsh <lwalsh_1980@hotmail.co.uk>
2022-01-07 23:48:08 +0000

    finished logic for product sizes

--- cart/views.py
+++ cart/views.py
@@ -1,23 +1,22 @@
 from django.shortcuts import render, redirect
 
+# Create your views here.
+
 
 def view_cart(request):
-    """ A view to renders the cart contents page """
+    """ A cart that renders the cart contents page """
 
     return render(request, 'cart/cart.html')
 
 
 def add_to_cart(request, item_id):
-    """ Add a quantity of the specified product to the shopping cart """
+    """ Add a quantity of the specified product to the shopping bag """
 
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     size = None
     if 'product_size' in request.POST:
-        size = request.POST['size']
-    color = None
-    if 'product_color' in request.POST:
-        color = request.POST['color']
+        size = request.POST['product_size']
     cart = request.session.get('cart', {})
 
     if size:
@@ -29,21 +28,6 @@ def add_to_cart(request, item_id):
         else:
             cart[item_id] = {'items_by_size': {size: quantity}}
     else:
-        if item_id in list(cart.keys()):
-            cart[item_id] += quantity
-        else:
-            cart[item_id] = quantity
-
-    if color:
-        if item_id in list(cart.keys()):
-            if color in cart[item_id]['items_by_color'].keys():
-                cart[item_id]['items_by_color'][color] += quantity
-            else:
-                cart[item_id]['items_by_color'][color] = quantity
-        else:
-            cart[item_id] = {'items_by_color': {color: quantity}}
-    else:
-
         if item_id in list(cart.keys()):
             cart[item_id] += quantity
         else:
