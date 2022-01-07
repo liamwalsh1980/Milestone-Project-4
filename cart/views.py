c4f23b70d423dded86ce3c3b1061629696712272
Liam Walsh <lwalsh_1980@hotmail.co.uk>
2022-01-07 09:26:00 +0000

    Started adding logic for sizes and colors

--- cart/views.py
+++ cart/views.py
@@ -12,12 +12,42 @@ def add_to_cart(request, item_id):
 
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
+    size = None
+    if 'product_size' in request.POST:
+        size = request.POST['size']
+    color = None
+    if 'product_color' in request.POST:
+        color = request.POST['color']
     cart = request.session.get('cart', {})
 
-    if item_id in list(cart.keys()):
-        cart[item_id] += quantity
+    if size:
+        if item_id in list(cart.keys()):
+            if size in cart[item_id]['items_by_size'].keys():
+                cart[item_id]['items_by_size'][size] += quantity
+            else:
+                cart[item_id]['items_by_size'][size] = quantity
+        else:
+            cart[item_id] = {'items_by_size': {size: quantity}}
     else:
-        cart[item_id] = quantity
+        if item_id in list(cart.keys()):
+            cart[item_id] += quantity
+        else:
+            cart[item_id] = quantity
+
+    if color:
+        if item_id in list(cart.keys()):
+            if color in cart[item_id]['items_by_color'].keys():
+                cart[item_id]['items_by_color'][color] += quantity
+            else:
+                cart[item_id]['items_by_color'][color] = quantity
+        else:
+            cart[item_id] = {'items_by_color': {color: quantity}}
+    else:
+
+        if item_id in list(cart.keys()):
+            cart[item_id] += quantity
+        else:
+            cart[item_id] = quantity
 
     request.session['cart'] = cart
     return redirect(redirect_url)
