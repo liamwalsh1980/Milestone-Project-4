1d120f0dc9043f1c6b00806e7941b58de53e4f11
Liam Walsh <lwalsh_1980@hotmail.co.uk>
2022-01-08 17:05:51 +0000

    Improved the quantity section in cart

--- cart/contexts.py
+++ cart/contexts.py
@@ -28,7 +28,7 @@ def cart_contents(request):
                 product_count += quantity
                 cart_items.append({
                     'item_id': item_id,
-                    'quantity': item_data,
+                    'quantity': quantity,
                     'product': product,
                     'size': size,
                 })
