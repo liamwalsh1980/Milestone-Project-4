dd709d0f1bde83727bd3612407b329f47fec6beb
Liam Walsh <lwalsh_1980@hotmail.co.uk>
2022-01-13 20:54:07 +0000

    Added Stripe payment elements

---
+++ checkout/static/checkout/js/stripe_elements.js
@@ -0,0 +1,28 @@
+/*
+    Core logic/payment flow for this comes from here:
+    https://stripe.com/docs/payments/accept-a-payment
+    CSS from here: 
+    https://stripe.com/docs/stripe-js
+*/
+
+var stripePublicKey = $('#id_stripe_public_key').text().slice(1, -1);
+var clientSecret = $('#id_client_secret').text().slice(1, -1);
+var stripe = Stripe(stripePublicKey);
+var elements = stripe.elements();
+var style = {
+    base: {
+        color: '#131418',
+        fontFamily: '"HUbuntu", Helvetica, sans-serif',
+        fontSmoothing: 'antialiased',
+        fontSize: '16px',
+        '::placeholder': {
+            color: '#aab7c4'
+        }
+    },
+    invalid: {
+        color: '#dc3545',
+        iconColor: '#dc3545'
+    }
+};
+var card = elements.create('card', {style: style});
+card.mount('#card-element');
--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51JwupGCAtXNYfKznaro0wSxp4eX36spSIIKYA8I1UOpudbc44EHFb6r4RpOziu5EzfClhEP6jPzXGW5VRhY9uCTb00TZ15SEVo',
+        'client_secret': 'test client secret'
     }
 
     return render(request, template, context)
