cf5b7da9a0c4109134f491c8d4b3789b99d9be6b
Liam Walsh <lwalsh_1980@hotmail.co.uk>
2022-01-17 20:53:55 +0000

    Added profile app

--- the_bike_shop/settings.py
+++ the_bike_shop/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'services',
     'cart',
     'checkout',
+    'profiles',
 
     # Other
     'crispy_forms',
--- the_bike_shop/urls.py
+++ the_bike_shop/urls.py
@@ -26,4 +26,5 @@ urlpatterns = [
     path('services/', include('services.urls')),
     path('cart/', include('cart.urls')),
     path('checkout/', include('checkout.urls')),
+    path('profile/', include('profiles.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
