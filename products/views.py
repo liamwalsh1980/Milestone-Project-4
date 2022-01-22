dd468a9f7040eb830e2e362be1c6588eec7af922
Liam Walsh <lwalsh_1980@hotmail.co.uk>
2022-01-22 15:35:41 +0000

    Delete product function added and edit and delete text links added to products and product detail pages

--- products/views.py
+++ products/views.py
@@ -119,3 +119,11 @@ def edit_product(request, product_id):
     }
 
     return render(request, template, context)
+
+
+def delete_product(request, product_id):
+    """ Delete a product from the store """
+    product = get_object_or_404(Product, pk=product_id)
+    product.delete()
+    messages.success(request, 'Product deleted!')
+    return redirect(reverse('products'))
