b754062c97e27b5874478c421dcb11858b6d3614
Liam Walsh <lwalsh_1980@hotmail.co.uk>
2022-01-28 17:00:28 +0000

    Updated services section with more features ready to complete checkout

--- book/views.py
+++ book/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404, HttpResponse
+from django.contrib import messages
 from services.models import Service
 
 
@@ -12,7 +13,7 @@ def service_booking(request):
     service = None
     if book:
         service = get_object_or_404(Service, id=book[0])
-    print(service)
+    print(book)
 
     context = {"book_service": service}
 
@@ -27,6 +28,29 @@ def add_to_booking(request, item_id):
 
     # print('Book =', book)
     request.session['book'] = [service.id]
+    messages.success(request, f'"{service}" added')
+
     print(service.id)
 
     return redirect(redirect_url)
+
+
+def remove_from_booking(request, item_id):
+    """Remove the service from the booking"""
+
+    try:
+        service = get_object_or_404(Service, pk=item_id)
+
+        book = request.session.get('book', {})
+        print(book)
+        print(service.name)
+
+        book.pop(item_id)
+        messages.success(request, f'{service} removed')
+
+        request.session['book'] = book
+        return HttpResponse(status=200)
+
+    except Exception as e:
+        messages.error(request, f'Error removing service: {e}')
+        return HttpResponse(status=500)
