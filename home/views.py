105c37dcf4d6376dac3cf84124f73c67c4125fa5
Liam Walsh <lwalsh_1980@hotmail.co.uk>
2022-02-03 21:40:03 +0000

    login required decorator added

--- home/views.py
+++ home/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
 
 def index(request):
@@ -7,7 +9,12 @@ def index(request):
     return render(request, 'home/index.html')
 
 
+# Decorator to wrap around the function to check user access before executing
+@login_required
 def admin(request):
     """ A view to return the index page """
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only store owners can access this.')
+        return redirect(reverse('home'))
 
     return render(request, 'home/admin.html')
