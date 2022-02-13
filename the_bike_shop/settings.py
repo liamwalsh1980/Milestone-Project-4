c58a43b06d181a98f4df42d3fde9510dd1b4dd55
Liam Walsh <lwalsh_1980@hotmail.co.uk>
2022-02-13 12:09:36 +0000

    Fixing images on homepage

--- the_bike_shop/settings.py
+++ the_bike_shop/settings.py
@@ -27,6 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
+
 ALLOWED_HOSTS = ['thebikeshop-project.herokuapp.com', 'localhost']
 
 
