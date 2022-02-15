d4f1ca80a2b2615f891631cbd1fd520c2591f341
Liam Walsh <lwalsh_1980@hotmail.co.uk>
2022-02-15 10:47:32 +0000

    Fixed linting issues for webhooks.py in checkout app

--- checkout/webhooks.py
+++ checkout/webhooks.py
@@ -26,10 +26,10 @@ def webhook(request):
         event = stripe.Webhook.construct_event(
             payload, sig_header, wh_secret
         )
-    except ValueError as e:
+    except ValueError:
         # Invalid payload
         return HttpResponse(status=400)
-    except stripe.error.SignatureVerificationError as e:
+    except stripe.error.SignatureVerificationError:
         # Invalid signature
         return HttpResponse(status=400)
     except Exception as e:
@@ -41,7 +41,8 @@ def webhook(request):
     # Map webhook events to relevant handler functions
     event_map = {
         'payment_intent.succeeded': handler.handle_payment_intent_succeeded,
-        'payment_intent.payment_failed': handler.handle_payment_intent_payment_failed,
+        'payment_intent.payment_failed': (
+            handler.handle_payment_intent_payment_failed),
     }
 
     # Get the webhook type from Stripe
